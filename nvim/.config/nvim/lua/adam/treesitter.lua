057f07bbf324db12bc5af5a9a88ae9646731762d
Adam Bremholm <adambremholm@gmail.com>
2022-09-23 09:39:18 +0200

    add bicep treesitter parser

--- nvim/.config/nvim/lua/adam/treesitter.lua
+++ nvim/.config/nvim/lua/adam/treesitter.lua
@@ -3,6 +3,18 @@ if not status_ok then
   return
 end
 
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.bicep = {
+  install_info = {
+    url = "~/.local/share/tree-sitter-bicep", -- local path or git repo
+    files = {"src/parser.c"},
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+}
+
 configs.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
