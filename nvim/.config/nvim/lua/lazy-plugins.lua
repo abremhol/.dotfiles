dd7080da96484cb9b6357a22bdd1cd1fca1cf775
Adam Bremholm <adambremholm@gmail.com>
2025-05-25 09:13:55 +0200

    - add neogit - Debug for dotnet

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -59,11 +59,11 @@ require('lazy').setup({
   require 'kickstart/plugins/easy-dotnet',
   require 'kickstart/plugins/copilot',
   require 'kickstart/plugins/copilot-chat',
+  require 'kickstart/plugins/neogit',
 
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
