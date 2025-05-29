fa82a81c66587d978093d5b000c58522f5533c1b
Adam Bremholm <adam.bremholm@volvocars.com>
2025-05-29 08:50:07 +0200

    adding some more copilot chat examples, fix tab not working in copilot chat

--- nvim/.config/nvim/lua/kickstart/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/copilot-chat.lua
@@ -1,3 +1,11 @@
+vim.keymap.set('n', '<leader>gct', vim.cmd.CopilotChatToggle, { desc = 'Toggle Copilot Chat' })
+-- to fix collision with tab from blink.cmp
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'copilot-chat',
+  callback = function()
+    vim.keymap.del('i', '<Tab>', { buffer = true })
+  end,
+})
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
@@ -7,6 +15,14 @@ return {
     },
     build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
+      completeopt = {
+        'popup', -- Show completion menu
+      },
+      mappings = {
+        complete = {
+          insert = '<C-å>',
+        },
+      },
       -- See Configuration section for options
     },
     -- See Commands section for default commands if you want to lazy load on them
