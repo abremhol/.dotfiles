a163ebe82cfddb37745047ff8ac5bad208935a76
Adam Bremholm <adam.bremholm@volvocars.com>
2025-06-01 10:15:51 +0200

    fixes

--- nvim/.config/nvim/lua/kickstart/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/copilot-chat.lua
@@ -1,16 +1,25 @@
 vim.keymap.set('n', '<leader>gct', vim.cmd.CopilotChatToggle, { desc = 'Toggle Copilot Chat' })
--- to fix collision with tab from blink.cmp
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'copilot-chat',
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuOpen',
   callback = function()
-    vim.keymap.del('i', '<Tab>', { buffer = true })
+    require('copilot.suggestion').dismiss()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuClose',
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
   end,
 })
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+
+      { 'zbirenbaum/copilot.lua' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
     build = 'make tiktoken', -- Only on MacOS or Linux
@@ -18,11 +27,11 @@ return {
       completeopt = {
         'popup', -- Show completion menu
       },
-      mappings = {
-        complete = {
-          insert = '<C-å>',
-        },
-      },
+      -- mappings = {
+      --   complete = {
+      --     insert = '<Tab>',
+      --   },
+      -- },
       -- See Configuration section for options
     },
     -- See Commands section for default commands if you want to lazy load on them
--- nvim/.config/nvim/lua/kickstart/plugins/copilot.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/copilot.lua
@@ -1,10 +1,71 @@
+-- Copilot.
 return {
-  'zbirenbaum/copilot.lua',
-  opts = {
+  {
+    'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
     event = 'InsertEnter',
-    config = function()
-      require('copilot').setup {}
-    end,
+    opts = {
+      -- I don't find the panel useful.
+      panel = { enabled = true },
+      suggestion = {
+        auto_trigger = true,
+        -- Use alt to interact with Copilot.
+        keymap = {
+          -- Disable the built-in mapping, we'll configure it in nvim-cmp.
+          accept = false,
+          accept_word = '<M-w>',
+          accept_line = '<M-l>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '/',
+        },
+      },
+      filetypes = {
+        markdown = true,
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+            -- disable for .env files
+            return false
+          end
+          return true
+        end,
+      },
+      config = function(_, opts)
+        local copilot = require 'copilot.suggestion'
+        local luasnip = require 'luasnip'
+
+        require('copilot').setup(opts)
+
+        local function set_trigger(trigger)
+          vim.b.copilot_suggestion_auto_trigger = trigger
+          vim.b.copilot_suggestion_hidden = not trigger
+        end
+
+        -- Hide suggestions when the completion menu is open.
+        --
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'BlinkCmpMenuOpen',
+          callback = function()
+            vim.b.copilot_suggestion_hidden = true
+          end,
+        })
+
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'BlinkCmpMenuClose',
+          callback = function()
+            vim.b.copilot_suggestion_hidden = false
+            set_trigger(not luasnip.expand_or_locally_jumpable())
+          end,
+        })
+
+        -- Disable suggestions when inside a snippet.
+        vim.api.nvim_create_autocmd('User', {
+          pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
+          callback = function()
+            set_trigger(not luasnip.expand_or_locally_jumpable())
+          end,
+        })
+      end,
+    },
   },
 }
