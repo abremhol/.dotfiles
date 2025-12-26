6b99d48eb8e0b3e8b320efcc642907452f92950c
Adam Bremholm <adambremholm@gmail.com>
2025-12-26 20:59:49 +0100

    find those diagnostics

--- nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
@@ -105,6 +105,7 @@ return {
       vim.keymap.set('n', '<Leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Find Word (fix this)' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind [G]rep' })
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [d]iagnosticts' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [b]uffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
       vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { desc = '[F]ind Fu[Z]zy' })
